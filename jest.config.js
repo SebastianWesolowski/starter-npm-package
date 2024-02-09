09c055373c31fa3c8e0f833a3a6e863abd038520
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2024-02-09 23:57:41 +0100

    feat: ✨ add configuration for jest tests

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,13 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.spec.ts'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/types/**/*.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.scss$': 'jest-scss-transform',
+  },
   globals: {
     'ts-jest': {
       diagnostics: false,
